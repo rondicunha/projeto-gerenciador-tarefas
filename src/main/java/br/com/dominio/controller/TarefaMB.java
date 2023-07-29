d0dbc5005d8f572561e58800c70d4e44d9ddb2c2
Rondinelle Cunha <rondinelle.cunha.101@ufrn.edu.br>
2023-07-29 15:43:18 -0300

    Finalizando correções e deixando os filtros funcionais

--- src/main/java/br/com/dominio/controller/TarefaMB.java
+++ src/main/java/br/com/dominio/controller/TarefaMB.java
@@ -1,6 +1,7 @@
 package br.com.dominio.controller;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -25,21 +26,40 @@ public class TarefaMB implements Serializable {
 	private Tarefa tarefa;
 	@Inject
 	private TarefaService service;
-	
 	private List<Tarefa> tarefas;
+	private Long numeroBusca;
+    private String tituloBusca;
+    private String prioridadeBusca;
+    private String responsavelBusca;
+    private List<Tarefa> tarefasFiltradas;
+    private List<Tarefa> tarefasConcluidas;
 	
 	@PostConstruct
 	public void carregar() {
 		tarefas = service.findAll();
+		tarefasFiltradas = tarefas;
+		tarefasConcluidas = new ArrayList<>();
 		
 		for (Iterator<Tarefa> iterator = tarefas.iterator(); iterator.hasNext();) {
 		    Tarefa item = iterator.next();
 		    
-		    if(item.getSituacao().equals("Concluída"))
+		    if(item.getSituacao().equals("Concluída")) {
+		    tarefasConcluidas.add(item);
 		    iterator.remove();
+		    }
+		    
 		}
 	}
 	
+
+	public List<Tarefa> getTarefasFiltradas() {
+		return tarefasFiltradas;
+	}
+
+	public void setTarefasFiltradas(List<Tarefa> tarefasFiltradas) {
+		this.tarefasFiltradas = tarefasFiltradas;
+	}
+
 	public String adicionar() {
 		try {
 			tarefa.setSituacao("Em andamento");
@@ -78,8 +98,83 @@ public class TarefaMB implements Serializable {
 			Message.erro(e.getMessage());
 		}
 	}
-	public String editar() {
+	public String paginaEditar() {
 		return "/EditarTarefa";
+	}
+	public String editar() {
+		try {
+			tarefa.setSituacao("Em andamento");
+			service.salvar(tarefa);
+			tarefa = new Tarefa();
+			carregar();
+			
+			Message.info("Salvo com sucesso!");
+		}catch(NegocioException e){
+			Message.erro(e.getMessage());
+		}
+		
+		return "/GerenciarTarefas";
+	}
+	
+	public List<Tarefa> filtrarTarefas() {
+		
+		System.out.print("Chegou aqui");
+		System.out.print(numeroBusca);
+		System.out.print(tituloBusca);
+		System.out.print(prioridadeBusca);
+		System.out.print(responsavelBusca);
+		
+		tarefasFiltradas = new ArrayList<>();
+		
+        for (Tarefa tarefa : tarefas) {
+            boolean correspondeNumero = (numeroBusca == null) || (tarefa.getId() == numeroBusca);
+            boolean correspondeTitulo = (tituloBusca == null) || tarefa.getTitulo().toLowerCase().contains(tituloBusca.toLowerCase());
+            boolean correspondePrioridade = ((prioridadeBusca == null) ||  prioridadeBusca.isEmpty()) || tarefa.getPrioridade().equalsIgnoreCase(prioridadeBusca);
+            boolean correspondeResponsavel = ((responsavelBusca == null) || responsavelBusca.isEmpty()) || tarefa.getResponsavel().equalsIgnoreCase(responsavelBusca);
+
+            if (correspondeNumero && correspondeTitulo && correspondePrioridade && correspondeResponsavel) {
+                tarefasFiltradas.add(tarefa);
+            }
+        }
+        System.out.print(tarefasFiltradas);
+        return tarefasFiltradas;
+    }
+	
+	public String cancelar() {
+		carregar();
+		return "/GerenciarTarefas";
+	}
+	
+	public Long getNumeroBusca() {
+		return numeroBusca;
+	}
+
+	public void setNumeroBusca(Long numeroBusca) {
+		this.numeroBusca = numeroBusca;
+	}
+
+	public String getTituloBusca() {
+		return tituloBusca;
+	}
+
+	public void setTituloBusca(String tituloBusca) {
+		this.tituloBusca = tituloBusca;
+	}
+
+	public String getPrioridadeBusca() {
+		return prioridadeBusca;
+	}
+
+	public void setPrioridadeBusca(String prioridadeBusca) {
+		this.prioridadeBusca = prioridadeBusca;
+	}
+
+	public String getResponsavelBusca() {
+		return responsavelBusca;
+	}
+
+	public void setResponsavelBusca(String responsavelBusca) {
+		this.responsavelBusca = responsavelBusca;
 	}
 
 	public Tarefa getTarefa() {
@@ -93,6 +188,19 @@ public class TarefaMB implements Serializable {
 	public List<Tarefa> getTarefas() {
 		return tarefas;
 	}
+
+	public List<Tarefa> getTarefasConcluidas() {
+		return tarefasConcluidas;
+	}
+
+	public void setTarefasConcluidas(List<Tarefa> tarefasConcluidas) {
+		this.tarefasConcluidas = tarefasConcluidas;
+	}
+
+	public void setTarefas(List<Tarefa> tarefas) {
+		this.tarefas = tarefas;
+	}
+	
 	
 	
 }
